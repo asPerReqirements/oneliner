6eacfbe301ebfb58828df7afeca7d8e8ad643e39
Abhishek Pathak <69761436+scorcism@users.noreply.github.com>
2023-08-12 16:13:13 +0530

    Update GitHub.jsx

--- src/brands/GitHub.jsx
+++ src/brands/GitHub.jsx
@@ -4,7 +4,7 @@ const GitHub = () => {
     return (
         <>
             <div>
-                <a href="//github.com/asPerRequirement/one-liner" target="_black" rel="noreferrer">
+                <a href="//github.com/asPerRequirement/oneliner" target="_black" rel="noreferrer">
                     <img className="github" src="/github.svg" alt="github" title="Github Repo" />
                 </a>
             </div>
