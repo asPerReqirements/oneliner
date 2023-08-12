9eb5f4ec5fe8bfdcdb5bd4925135e711da1aee36
Abhishek Pathak <69761436+scorcism@users.noreply.github.com>
2023-08-12 16:13:31 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="footer">
                 <footer>
                     <Action />
-                    <a href="//github.com/asPerRequirements/one-liner" className="underline-animation" rel="noreferrer">asPerRequirements</a>
+                    <a href="//github.com/asPerRequirements/oneliner" className="underline-animation" rel="noreferrer">asPerRequirements</a>
                     <p className="small"><a href="https://opensource.org/" className="" target="_blank" rel="noreferrer">Open Source Project</a> by <a href="//github.com/scorcism" rel="noreferrer" target="_black">Abhishek Pathak</a></p>
                 </footer>
             </div>
